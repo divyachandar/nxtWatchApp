912bf3293050d93eca06b921b77d659d97003ad9
divyachandar <dvrmahanty@gmail.com>
2023-07-25 11:44:35 +0000

    nxtWatch commit

---
+++ src/components/TrendingVideoCard/index.js
@@ -0,0 +1,65 @@
+import NxtWatchContext from '../../context/NxtWatchContext'
+
+import {
+  ItemLink,
+  TrendingListItem,
+  TrendingThumbNailImage,
+  TrendingVideoDetails,
+  TrendingProfileImage,
+  TrendingContentSection,
+  TrendingTitle,
+  TrendingChannelName,
+  TrendingViewsAndDate,
+  TrendingDot,
+} from './styledComponents'
+
+const TrendingVideoCard = props => {
+  const {videoDetails} = props
+  const {
+    id,
+    title,
+    thumbnailUrl,
+    viewCount,
+    publishedAt,
+    name,
+    profileImageUrl,
+  } = videoDetails
+
+  return (
+    <NxtWatchContext.Consumer>
+      {value => {
+        const {isDarkTheme} = value
+        const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+        return (
+          <ItemLink to={`/videos/${id}`} className="link">
+            <TrendingListItem>
+              <TrendingThumbNailImage
+                src={thumbnailUrl}
+                alt="video thumbnail"
+              />
+              <TrendingVideoDetails>
+                <TrendingProfileImage
+                  src={profileImageUrl}
+                  alt="channel logo"
+                />
+                <TrendingContentSection>
+                  <TrendingTitle color={textColor}>{title}</TrendingTitle>
+                  <TrendingChannelName color={textColor}>
+                    {name}
+                  </TrendingChannelName>
+                  <TrendingViewsAndDate color={textColor}>
+                    {viewCount} views<TrendingDot> &#8226; </TrendingDot>
+                    {publishedAt}
+                  </TrendingViewsAndDate>
+                </TrendingContentSection>
+              </TrendingVideoDetails>
+            </TrendingListItem>
+          </ItemLink>
+        )
+      }}
+    </NxtWatchContext.Consumer>
+  )
+}
+
+export default TrendingVideoCard
